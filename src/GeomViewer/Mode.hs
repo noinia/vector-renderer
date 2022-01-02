bf1b6aaaa12e837a5b531fc2afc24dc00e71aa23
Frank Staals <frank@fstaals.net>
2022-01-02 19:06:30 +0100

    geomviewer stuff

---
+++ src/GeomViewer/Mode.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE TypeFamilyDependencies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TemplateHaskell #-}
+module GeomViewer.Mode where
+
+import           Control.Lens
+import           Data.Default
+import           Data.Geometry.Box
+import           Data.Geometry.Point
+import           Data.Geometry.PolyLine
+import           Data.Geometry.Polygon
+import           Data.Map (Map)
+import qualified Data.Map as Map
+import           Data.Vinyl
+import           Data.Vinyl.CoRec
+import           Data.Vinyl.TypeLevel
+
+--------------------------------------------------------------------------------
+
+data PartialPolyLine r = StartPoint (Point 2 r)
+                       | PartialPolyLine (PolyLine 2 () r)
+                       deriving (Show,Eq)
+makePrisms ''PartialPolyLine
+
+data PartialPolygon r = SinglePoint (Point 2 r)
+                      | TwoPoints (Point 2 r) (Point 2 r)
+                      | MultiplePoints (PolyLine 2 () r)
+                      deriving (Show,Eq)
+makePrisms ''PartialPolygon
+
+--------------------------------------------------------------------------------
+
+data Mode s r = SelectMode (Maybe s)
+              -- ^ the selected item, if any
+              | PointMode
+              | PolyLineMode (Maybe (PartialPolyLine r))
+                -- ^ the points that we already placed
+              | PolygonMode  (Maybe (PartialPolygon r))
+              deriving (Show,Eq)
+makePrisms ''Mode
+
+instance Default (Mode s r) where
+  def = SelectMode Nothing
+
+--------------------------------------------------------------------------------
+
+data GeomUniverse = SPoint
+                  | SPolyLine
+                  | SSimplePolygon
+                  | SRectangle
+                  deriving (Show,Eq)
+
+type family GeomUniverseF r u = (result :: *) | result -> u where
+  GeomUniverseF r SPoint         = Point 2 r
+  GeomUniverseF r SPolyLine      = PolyLine 2 () r
+  GeomUniverseF r SSimplePolygon = SimplePolygon () r
+  GeomUniverseF r SRectangle     = Rectangle () r
+
+newtype Attr f r u = Attr { _unAttr :: f (GeomUniverseF r u) }
+
+makeLenses ''Attr
+
+deriving instance Show      (f (GeomUniverseF r u)) => Show (Attr f r u)
+deriving instance Eq        (f (GeomUniverseF r u)) => Eq   (Attr f r u)
+deriving instance Semigroup (f (GeomUniverseF r u)) => Semigroup   (Attr f r u)
+deriving instance Monoid    (f (GeomUniverseF r u)) => Monoid      (Attr f r u)
+
+
+newtype GeometryCollection f gs r =
+  GeometryCollection { _toRec ::  Rec (Attr f r) gs }
+makeLenses ''GeometryCollection
+
+
+instance (Semigroup (Rec (Attr f r) gs)) => Semigroup (GeometryCollection f gs r) where
+  (GeometryCollection a) <> (GeometryCollection b) = GeometryCollection $ a <> b
+
+
+deriving instance (RMap gs, ReifyConstraint Show (Attr f r) gs, RecordToList gs)
+                  => Show (GeometryCollection f gs r)
+
+-- | add an element to the collection
+insertWith       :: RElem u gs (RIndex u gs)
+                 => (GeomUniverseF r u -> f (GeomUniverseF r u) -> f (GeomUniverseF r u))
+                 -> GeomUniverseF r u -> GeometryCollection f gs r -> GeometryCollection f gs r
+insertWith f x c = c&toRec.rlens.unAttr %~ f x
+
+
+
+insert :: RElem u gs (RIndex u gs)
+       => GeomUniverseF r u -> GeometryCollection [] gs r -> GeometryCollection [] gs r
+insert = insertWith (:)
+
+
+
+
+--------------------------------------------------------------------------------
+
+data Model gs r = Model { _mode   :: Mode (CoRec (Attr Identity r) gs) r
+                        , _layers :: Map String (GeometryCollection [] gs r)
+                        }
+makeLenses ''Model
+
+deriving instance ( Show  (CoRec (Attr Identity r) gs)
+                  , Show (GeometryCollection [] gs r), Show r
+                  ) => Show (Model gs r)
+
+--------------------------------------------------------------------------------
+
+
+
+
+--------------------------------------------------------------------------------
+-- * View
---
+++ src/VectorRenderer/GeomViewer.hs
@@ -0,0 +1,135 @@
+{-# LANGUAGE OverloadedStrings     #-}
+module VectorRenderer.NestedViewport where
+
+import Cairo.Canvas.Ipe
+import Cairo.Canvas.Primitives
+import Control.Lens
+import Control.Monad (void)
+import Data.Default
+import Data.Ext
+import Data.Geometry.Box
+import Data.Geometry.Point
+import Data.Geometry.Transformation
+import Data.RealNumber.Rational
+import Debug.Trace
+import Graphics.Rendering.Cairo.Canvas (Canvas)
+import Ipe
+import Ipe.Color
+import Reflex
+import Reflex.SDL2 hiding (point, origin, Rectangle, Point)
+import SDL.GeometryUtil
+import SDL.Util
+import System.Random
+import UI.Layout
+import GeomViewer.Viewport
+import GeomViewer.Viewport.Dynamic.SDL
+import VectorRenderer.ReflexSDLRenderer
+import VectorRenderer.RenderCanvas
+
+import Data.Geometry.PolyLine
+import Data.Geometry.Polygon
+import Data.Map(Map)
+import qualified Data.Map as Map
+import Data.Vinyl
+import Data.Vinyl.CoRec
+
+--------------------------------------------------------------------------------
+
+type R = RealNumber 5
+
+--------------------------------------------------------------------------------
+
+randomPoint :: IO (Point 2 R :+ ())
+randomPoint = (\x y -> ext . fmap realToFrac $ Point2 x y)
+              <$> randomRIO @Int (0,300)
+              <*> randomRIO (0,300)
+
+--------------------------------------------------------------------------------
+
+
+
+
+
+
+--------------------------------------------------------------------------------
+
+-- | Main reflex app that can also render layers
+reflexMain :: (ReflexSDL2Renderer t m Double) => m ()
+reflexMain = do
+               drawLayer . pure $ ipeObject . iO $ defIO (Point2 0 0) ! attr SStroke black
+               let r = box (ext $ Point2 10 10) (ext $ Point2 200 300)
+               drawLayer . pure $ ipeObject . iO $ defIO r ! attr SFill red
+
+
+
+               drawLayer . pure $ roundedRectangle' (Just red)
+                                                    (Just blue)
+                                                    20 (box (ext $ Point2 100 700)
+                                                            (ext $ Point2 400 800))
+
+
+               -- dz <- zoomDyn defaultZoomConfig
+               -- performEvent_ $ ffor (updated dz) $ \z ->
+               --   liftIO $ print z
+
+               dViewport <- pannableZoomableViewportDyn def myViewport
+               -- dViewport <- zoomableViewportDyn' def myViewport
+
+
+               let drawStuff = do
+                     ipeObject . iO $ defIO (traceShowId $ Point2 0 0) ! attr SStroke blue
+                     ipeObject . iO $ defIO r ! attr SStroke blue
+                     void $ textAt origin "foo"
+
+               drawLayer $ flip drawInViewport drawStuff <$> dViewport
+
+               let screen = box (ext $ Point2 0 0) (ext $ Point2 1440 810)
+               drawLayer . pure $ renderLayout' screen myLayout
+
+
+               -- show a point at the mouse pos
+               dMousePos <- mousePositionDyn
+               let dMousePosDrawing = ffor dMousePos $ \case
+                     Nothing         -> pure () -- don't draw anything
+                     Just (p :+ dat) -> let color = if null (mouseMotionEventState dat)
+                                                    then black
+                                                    else blue
+                                        in colored point (p :+ color)
+               drawLayer dMousePosDrawing
+
+-- | Viewport in which everything is rotated 90 deg.
+myViewport :: Viewport Double
+myViewport = mkViewport (box (ext $ Point2 100 100) (ext $ Point2 500 500))
+                        Data.Geometry.Transformation.identity -- (rotation $ pi / 2 )
+
+-- | Draws the content in the viewport
+drawViewport          :: RealFrac r => Viewport r -> Canvas ()
+drawViewport viewport = ipeObject . iO $ defIO (viewport^.viewPort) ! attr SStroke black
+
+-- | Draws the content in the viewport
+drawInViewport              :: RealFrac r => Viewport r -> Canvas () -> Canvas ()
+drawInViewport viewport act = do drawViewport viewport
+                                 renderInViewport viewport act
+
+
+--------------------------------------------------------------------------------
+
+
+
+
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+  initializeAll
+  let ogl = defaultOpenGL{ glProfile = Core Debug 3 3 }
+      cfg = defaultWindow{ windowGraphicsContext = OpenGLContext ogl
+                         , windowResizable       = True
+                         -- , windowHighDPI         = False
+                         , windowInitialSize     = V2 1440 810
+                         }
+  withWindow "convex hull" cfg $ \window -> do
+    void $ glCreateContext window
+    withRenderer window (-1) defaultRenderer $ \renderer -> do
+      host $ reflexSdlApp window renderer reflexMain
+  quit
